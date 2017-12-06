ba898bcdefa57b005412b78fb15595072d488fbe
John Harvey <john.p.harvey@btinternet.com>
2017-12-06 21:33:38 +0000

    Update copyright notices to those from cgateinterface

--- addons/binding/org.openhab.binding.cbus/src/main/java/org/openhab/binding/cbus/internal/cgate/DebugEventCallback.java
+++ addons/binding/org.openhab.binding.cbus/src/main/java/org/openhab/binding/cbus/internal/cgate/DebugEventCallback.java
@@ -1,10 +1,19 @@
-/**
- * Copyright (c) 2010-2017 by the respective copyright holders.
+/*
+ * CGateInterface - A library to allow interaction with Clipsal C-Gate.
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright 2008, 2009, 2012, 2017 Dave Oxley <dave@daveoxley.co.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.openhab.binding.cbus.internal.cgate;
 
